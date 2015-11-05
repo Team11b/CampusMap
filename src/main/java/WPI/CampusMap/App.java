1c0c6568ed2adfc80123a68d9ed79c8fc9d7a3cc
Will Spurgeon <wespurgeon@wpi.edu>
2015-11-05 12:57:09 -0500

    Dependencies Fixed
    The Build dependencies have been fixed. Windows still refuse to open
    though.

--- src/main/java/WPI/CampusMap/App.java
+++ src/main/java/WPI/CampusMap/App.java
@@ -1,4 +1,5 @@
 package WPI.CampusMap;
+import javax.swing.*;
 
 /**
  * Hello world!
@@ -8,7 +9,14 @@ public class App
 {
     public static void main( String[] args )
     {
-    	MainViewFrame.main(args);
+    	JFrame frame = new JFrame("HelloWorldSwing");
+        final JLabel label = new JLabel("Hello World");
+        frame.getContentPane().add(label);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
         System.out.println( "This is a CI Test" );
         
     }
