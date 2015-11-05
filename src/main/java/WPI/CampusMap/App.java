d3813ada08d20f1055ab01f63623549e50cac9a6
Will Spurgeon <wespurgeon@wpi.edu>
2015-11-05 14:40:23 -0500

    Separator
    Made the window larger, re-arranged some buttons and added a separator
    object, although i don’t know where it is now.

--- src/main/java/WPI/CampusMap/App.java
+++ src/main/java/WPI/CampusMap/App.java
@@ -6,6 +6,10 @@ import java.awt.BorderLayout;
 import javax.swing.JPanel;
 import javax.swing.JToggleButton;
 import javax.swing.JButton;
+import javax.swing.JSeparator;
+import javax.swing.SwingConstants;
+import java.awt.Color;
+import java.awt.Dimension;
 
 /**
  * Hello world!
@@ -19,40 +23,51 @@ public class App
     	frame.getContentPane().setLayout(null);
     	
     	JPanel mainPanel = new JPanel();
-    	mainPanel.setBounds(6, 6, 252, 310);
+    	mainPanel.setBounds(6, 6, 539, 555);
     	frame.getContentPane().add(mainPanel);
     	mainPanel.setLayout(null);
     	
     	JButton btnMap = new JButton("Map");
-    	btnMap.setBounds(98, 5, 75, 29);
+    	btnMap.setBounds(402, 5, 75, 29);
     	mainPanel.add(btnMap);
     	
     	JButton btnEdit = new JButton("Edit");
-    	btnEdit.setBounds(171, 5, 75, 29);
+    	btnEdit.setBounds(464, 5, 75, 29);
     	mainPanel.add(btnEdit);
     	
     	JButton btnGetDirections = new JButton("Get Directions");
-    	btnGetDirections.setBounds(112, 275, 134, 29);
+    	btnGetDirections.setBounds(399, 520, 134, 29);
     	mainPanel.add(btnGetDirections);
     	
     	JLabel lblMapviewGoesHere = new JLabel("MapView goes here.");
-    	lblMapviewGoesHere.setBounds(48, 149, 146, 16);
+    	lblMapviewGoesHere.setBounds(218, 286, 146, 16);
     	mainPanel.add(lblMapviewGoesHere);
     	
     	JPanel directionsPanel = new JPanel();
-    	directionsPanel.setBounds(270, 6, 118, 310);
+    	directionsPanel.setBounds(557, 6, 237, 555);
     	frame.getContentPane().add(directionsPanel);
     	directionsPanel.setLayout(null);
     	
     	JLabel lblDirections = new JLabel("Directions\ngo here");
-    	lblDirections.setBounds(6, 6, 106, 41);
+    	lblDirections.setBounds(6, 6, 225, 41);
     	directionsPanel.add(lblDirections);
     	
     	JButton btnEmail = new JButton("Email");
-    	btnEmail.setBounds(-5, 275, 117, 29);
+    	btnEmail.setBounds(0, 520, 117, 29);
     	directionsPanel.add(btnEmail);
     	
-    	frame.setSize(500, 500);
+    	JButton btnPrint = new JButton("Print");
+    	btnPrint.setBounds(114, 520, 117, 29);
+    	directionsPanel.add(btnPrint);
+    	
+    	JSeparator separator = new JSeparator();
+    	separator.setBackground(Color.RED);
+    	separator.setOrientation(SwingConstants.VERTICAL);
+    	separator.setPreferredSize(new Dimension(50,10));
+    	//separator.setBounds(100, 100, 174, 246);
+    	frame.getContentPane().add(separator);
+    	
+    	frame.setSize(800, 600);
     	frame.setVisible(true);
 
         
