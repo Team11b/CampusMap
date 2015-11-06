ad375680dea2cc589dd77cb711132981411aade2
jlzizmor <jlzizmor@wpi.edu>
2015-11-06 13:14:56 -0500

    commit Map with A* that does not compile so it can be merged with full Map class

--- src/main/java/WPI/CampusMap/AStar/Map.java
+++ src/main/java/WPI/CampusMap/AStar/Map.java
@@ -1,7 +1,41 @@
 package WPI.CampusMap.AStar;
 
+import java.util.ArrayList;
+
 public class Map {
+	
 	public Map() {
 		
+	}
+	
+	/**
+	 * Creates a Path of points using the A* algorithm.
+	 * Uses this map as a map.
+	 * Will return null if either the start or the goal is an invalid location, a wall.
+	 * @param start the starting Point
+	 * @param goal the goal Point
+	 * @return a Path of points or null if either the start or goal is invalid
+	 */
+	public Path astar(Point start, Point goal) {
+		// checks to see if either the start or goal is a wall
+		if (start.getType() == Point.WALL) {
+			System.out.println("Invalid start point.");
+			return null;
+		}
+		else if (goal.getType() == Point.WALL) {
+			System.out.println("Invalid goal point.");
+			return null;
+		}
+		
+		// Instantiate frontier and explored lists
+		ArrayList<Node> frontier = new ArrayList<Node>();
+		ArrayList<Node> explored = new ArrayList<Node>();
+		
+		// add start to frontier as a Node
+		frontier.add(new Node(start, null, 0));
+		
+		while (!frontier.isEmpty()) {
+			
+		}
 	}
 }
