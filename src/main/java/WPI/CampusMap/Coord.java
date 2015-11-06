bec7315ce4bc19930101bc012c12659ff86ebeb7
Will Spurgeon <wespurgeon@wpi.edu>
2015-11-06 15:24:40 -0500

    Distance is static
    If distance() is going to be implemented as distance(coord1, coord2),
    the method should be static.

--- src/main/java/WPI/CampusMap/Coord.java
+++ src/main/java/WPI/CampusMap/Coord.java
@@ -9,7 +9,7 @@ public class Coord {
 		this.y = y;
 	}
 
-	public float distance(Coord p1, Coord p2) {
+	public static float distance(Coord p1, Coord p2) {
 		return (float) Math.sqrt(Math.pow(p2.getY() - p1.getY(), 2) + Math.pow(p2.getX() - p1.getX(), 2));
 	}
 
