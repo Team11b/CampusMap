6d4eb4f0ae0eea3d8cc4c990fb2795c476df864c
Max Stenke <max_stenke@yahoo.de>
2015-11-06 13:09:43 -0500

    Add POI class.

---
+++ src/main/java/WPI/CampusMap/AStar/POI.java
@@ -0,0 +1,19 @@
+package WPI.CampusMap.AStar;
+
+public class POI {
+	private Map linkedMap;
+	
+	public POI(Map linkedMap){
+		this.linkedMap = linkedMap;
+	}
+
+	public Map getLinkedMap() {
+		return linkedMap;
+	}
+
+	public void setLinkedMap(Map linkedMap) {
+		this.linkedMap = linkedMap;
+	}
+	
+	
+}
