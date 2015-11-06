38aebc57db858e82ac48e979b3ace91b190bed8a
Max Stenke <max_stenke@yahoo.de>
2015-11-06 12:27:38 -0500

    Add coordinate class

---
+++ src/main/java/WPI/CampusMap/Coord.java
@@ -0,0 +1,33 @@
+package WPI.CampusMap;
+
+public class Coord {
+	private float x;
+	private float y;
+
+	public Coord(float x, float y) {
+		this.x = x;
+		this.x = y;
+	}
+
+	public float distance(Coord p1, Coord p2) {
+		return (float) Math.sqrt(Math.pow(2, p2.getY() + p1.getY()) + Math.pow(2, p2.getX() + p1.getX()));
+
+	}
+
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+
+}
