cc3401a765c2e7b5793b0c6cd77d66b0cc4960f8
Max Stenke <max_stenke@yahoo.de>
2015-11-06 13:05:37 -0500

    Update Map and Path class.

--- src/main/java/WPI/CampusMap/AStar/Map.java
+++ src/main/java/WPI/CampusMap/AStar/Map.java
@@ -1,7 +1,38 @@
 package WPI.CampusMap.AStar;
 
 public class Map {
-	public Map() {
-		
+
+	private String png;
+	private Point[] map;
+
+	public Map(String png, Point[] map) {
+		this.png = png;
+		this.map = map;
 	}
+
+	public Path astar(Point start, Point end) {
+		Path temp = null;
+		return temp; // TODO: astar();
+	}
+
+	public void expandWalls() {
+		// TODO: expandWalls();
+	}
+
+	public String getPng() {
+		return png;
+	}
+
+	public void setPng(String png) {
+		this.png = png;
+	}
+
+	public Point[] getMap() {
+		return map;
+	}
+
+	public void setMap(Point[] map) {
+		this.map = map;
+	}
+
 }
