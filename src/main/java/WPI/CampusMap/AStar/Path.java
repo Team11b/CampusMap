5458abc6f589a62447bf4be9fb3186c587b58751
Max Stenke <max_stenke@yahoo.de>
2015-11-08 13:25:43 -0500

    Add getTurns methon in Path class. Still wip.

--- src/main/java/WPI/CampusMap/AStar/Path.java
+++ src/main/java/WPI/CampusMap/AStar/Path.java
@@ -10,33 +10,82 @@ public class Path {
 	public Path(ArrayList<Node> path) {
 		this.path = path;
 	}
-	
+
 	public Path() {
 		this.path = new ArrayList<Node>();
 	}
-	
-	public boolean addNode(Node node){
+
+	public boolean addNode(Node node) {
 		return path.add(node);
 	}
-	
+
 	public void reverse() {
 		Collections.reverse(this.path);
 	}
 
+	/**
+	 * This method converts the A* star path into a clean path
+	 * 
+	 * @return an arraylist of nodes
+	 */
+
 	public Path getTurns() {
 		ArrayList<Node> temp = new ArrayList<Node>();
 		Node first = path.get(0);
-		Node last = path.get(path.size());
 
 		temp.add(first);
 		for (int i = 1; i < path.size(); i++) {
-			
+			if (path.get(i + 1) != null) {
+				// big chunk of if statements start
+				// check if next point is on the same level as i - 1 and i + 1
+				if ((getNodePointCoord(path.get(i)).getX() == getNodePointCoord(path.get(i + 1)).getX())
+						&& (getNodePointCoord(path.get(i)).getX() == getNodePointCoord(path.get(i - 1)).getX())) {
+					System.out.println("Abridge one node horizontal");
+					break;
+				} else if ((getNodePointCoord(path.get(i)).getY() == getNodePointCoord(path.get(i + 1)).getY())
+						&& (getNodePointCoord(path.get(i)).getY() == getNodePointCoord(path.get(i - 1)).getY())) {
+					System.out.println("Abridge one node vertical");
+					break;
+				} else {
+					Coord deltaBefore = new Coord(
+							Math.abs(getNodePointCoord(path.get(i)).getX() - getNodePointCoord(path.get(i - 1)).getX()),
+							Math.abs(
+									getNodePointCoord(path.get(i)).getY() - getNodePointCoord(path.get(i - 1)).getY()));
+					Coord deltaAfter = new Coord(
+							Math.abs(getNodePointCoord(path.get(i)).getX() - getNodePointCoord(path.get(i + 1)).getX()),
+							Math.abs(
+									getNodePointCoord(path.get(i)).getY() - getNodePointCoord(path.get(i + 1)).getY()));
+					if ((deltaBefore.getX() == deltaAfter.getX()) && (deltaBefore.getY() == deltaAfter.getY())) {
+						System.out.println("Abridge one node vertical");
+						break;
+					}
+				}
+				// big chunk of if statements start
+			} else {
+				temp.add(path.get(i));
+			}
 		}
-		
+
 		return null;
 	}
-	
-	public Coord getNodePointCoord(Node node){
+
+	public static void main(String[] args) {
+		ArrayList<Node> turns = new ArrayList<Node>();
+		turns.add(new Node(new Point(new Coord(0, 0), null, null), null));
+		turns.add(new Node(new Point(new Coord(1, 0), null, null), null));
+		turns.add(new Node(new Point(new Coord(2, 0), null, null), null));
+		turns.add(new Node(new Point(new Coord(3, 0), null, null), null));
+		turns.add(new Node(new Point(new Coord(4, 1), null, null), null));
+		turns.add(new Node(new Point(new Coord(5, 2), null, null), null));
+		turns.add(new Node(new Point(new Coord(6, 2), null, null), null));
+		turns.add(new Node(new Point(new Coord(6, 3), null, null), null));
+		turns.add(new Node(new Point(new Coord(5, 3), null, null), null));
+		Path path = new Path(turns);
+		path.pathToString(path);
+		path.pathToString(path.getTurns());
+	}
+
+	public Coord getNodePointCoord(Node node) {
 		return node.getPoint().getCoord();
 	}
 
@@ -46,6 +95,13 @@ public class Path {
 
 	public void setPath(ArrayList<Node> path) {
 		this.path = path;
+	}
+
+	public void pathToString(Path path) {
+		for (int i = 0; i < path.getPath().size(); i++) {
+			System.out.print("(" + path.getPath().get(i).getPoint().getCoord().getX() + ","
+					+ path.getPath().get(i).getPoint().getCoord().getX() + ")");
+		}
 	}
 
 }
