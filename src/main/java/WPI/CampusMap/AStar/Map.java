20e88259ced0de797f34c1d2a02de3f3143c8993
Chris Cormier <chris.hw.cormier@gmail.com>
2015-11-15 17:01:28 -0500

    Fixed JavaDoc for map constructor

--- src/main/java/WPI/CampusMap/AStar/Map.java
+++ src/main/java/WPI/CampusMap/AStar/Map.java
@@ -35,6 +35,8 @@ public class Map {
 	 * @param scale Scale of this map
 	 * @param png File name of the image for this map
 	 * @param xml File name of the XML of points for this map
+	 * @throws FileNotFoundException
+	 * @throws XMLStreamException
 	 */
 	public Map(int scale, String png, String xml) throws FileNotFoundException,XMLStreamException{
 		this.scale = scale;
