cc071943ae37e8b7d1614b83b26ba7bb60264cc2
jlzizmor <jlzizmor@wpi.edu>
2015-11-21 17:14:35 -0500

    XML to Serialized data works

--- src/main/java/WPI/CampusMap/XML/XML.java
+++ src/main/java/WPI/CampusMap/XML/XML.java
@@ -169,6 +169,8 @@ public class XML {
 				}
 				if("Map".equals(reader.getLocalName()))
 				{
+					map.setName(map.getXML().substring(4, map.getXML().length()-4));
+					map.setPng(reader.getAttributeValue(0));
 					map.setScale(Integer.parseInt(reader.getAttributeValue(1)));
 				}
 				break;
--- src/main/java/XMLtoSER/Converter.java
+++ src/main/java/XMLtoSER/Converter.java
@@ -1,11 +1,16 @@
 package XMLtoSER;
 
 import java.io.File;
+import java.util.Arrays;
+
+import javax.xml.stream.XMLStreamException;
 
 import WPI.CampusMap.AStar.Map;
+import WPI.CampusMap.Serialization.Serialization;
+import WPI.CampusMap.XML.XML;
 
 public class Converter {
-	String[] ignore = {"5x5Test.xml", "5x5Test2.xml", "AK.xml", "borked.xml", "testOutput.xml"};
+	static String[] ignore = {"5x5Test.xml", "5x5Test2.xml", "AK.xml", "borked.xml", "testOutput.xml"};
 	
 	public static String[] getFileNames() {
 		File folder = new File("XML/");
@@ -20,6 +25,23 @@ public class Converter {
 	}
 	
 	public static void main(String[] args) {
-		Map temp = new Map();		
+		Map temp = new Map();
+		String[] files = Converter.getFileNames();
+		
+		for (int j = 0; j < files.length; j++) {
+			if (!(Arrays.asList(Converter.ignore).contains(files[j]))) {
+				temp = new Map();
+				temp.setXML("XML/" + files[j]);
+				
+				try {
+					temp.setMap(XML.parseXML(temp));
+				} catch (XMLStreamException e) {
+					e.printStackTrace();
+				}
+				System.out.println(temp.getName());
+				
+				Serialization.write(temp);
+			}
+		}
 	}
 }
