170bc23ccaf14d260f92f7d9871a4c64010b32df
Chris Cormier <chris.hw.cormier@gmail.com>
2015-11-22 14:17:56 -0500

    Fixed Serialization test to use HashMaps

--- src/test/java/WPI/CampusMap/SerializationTest/SerializationTest.java
+++ src/test/java/WPI/CampusMap/SerializationTest/SerializationTest.java
@@ -1,6 +1,7 @@
 package WPI.CampusMap.SerializationTest;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.junit.Test;
 
@@ -15,14 +16,14 @@ public class SerializationTest {
 	public void test() {
 		Map tempM = new Map();
 		tempM.setName("test_map");
-		ArrayList<Point> tempP = new ArrayList<Point>();
-		tempP.add(new Point(new Coord((float) 1.1, 0), "hiya", "id"));
+		HashMap<String, Point> tempP = new HashMap<String, Point>();
+		tempP.put("hiya", new Point(new Coord((float) 1.1, 0), "hiya", "id"));
 		tempM.setMap(tempP);
 
 		Serialization.write(tempM);
 
 		Map temp2 = Serialization.read(tempM.getName());
-		System.out.println("Output:\t\t\t" + temp2.getName() + "\t" + temp2.getMap().get(0).getCoord().getX());
+		System.out.println("Output:\t\t\t" + temp2.getName() + "\t" + temp2.getMap().get("hiya").getCoord().getX());
 		System.out.println("Expected:\t\tnew_map\t1.1");
 	}
 
