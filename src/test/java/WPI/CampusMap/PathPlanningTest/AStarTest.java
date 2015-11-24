01108ce3b7dbf4ccf4ba477bf005a5608a14338f
jlzizmor <jlzizmor@wpi.edu>
2015-11-24 18:49:27 -0500

    all a* working

--- src/test/java/WPI/CampusMap/PathPlanningTest/AStarTest.java
+++ src/test/java/WPI/CampusMap/PathPlanningTest/AStarTest.java
@@ -319,7 +319,6 @@ public class AStarTest {
 		Serializer.write(testMap6);
 	}
 
-	@Ignore
 	@Test
 	public void testAStar4to12() {
 		Point start, goal;
@@ -338,7 +337,6 @@ public class AStarTest {
 		assertEquals(pathNodes.get(6).getPoint().getId(), "12");
 	}
 
-	@Ignore
 	@Test
 	public void testAStar4to11() {
 		Point start, goal;
@@ -350,16 +348,15 @@ public class AStarTest {
 
 		assertEquals(path.getPath().size(), 8);
 		assertEquals(pathNodes.get(0).getPoint().getId(), "4");
-		assertEquals(pathNodes.get(1).getPoint().getId(), "3");
-		assertEquals(pathNodes.get(2).getPoint().getId(), "2");
-		assertEquals(pathNodes.get(3).getPoint().getId(), "1");
-		assertEquals(pathNodes.get(4).getPoint().getId(), "0");
-		assertEquals(pathNodes.get(5).getPoint().getId(), "5");
-		assertEquals(pathNodes.get(6).getPoint().getId(), "10");
+		assertEquals(pathNodes.get(1).getPoint().getId(), "9");
+		assertEquals(pathNodes.get(2).getPoint().getId(), "14");
+		assertEquals(pathNodes.get(3).getPoint().getId(), "19");
+		assertEquals(pathNodes.get(4).getPoint().getId(), "18");
+		assertEquals(pathNodes.get(5).getPoint().getId(), "17");
+		assertEquals(pathNodes.get(6).getPoint().getId(), "12");
 		assertEquals(pathNodes.get(7).getPoint().getId(), "11");
 	}
 
-	@Ignore
 	@Test
 	public void testAStar4to12_2() {
 		Point start, goal;
@@ -382,7 +379,6 @@ public class AStarTest {
 	@Test
 	public void testConnected() {
 		Map.addMap(testMap5);
-		Map.addMap(testMap6);
 		Point start, goal;
 		start = testMap5.getPoint("0");
 		goal = testMap6.getPoint("12");
