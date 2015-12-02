71765fe3c37abdbf81463d696dc875cf492c6196
Will Spurgeon <wespurgeon@wpi.edu>
2015-12-01 21:04:29 -0500

    The directions text area is now scrollable.

--- src/main/java/WPI/CampusMap/UI/AppUIObject.java
+++ src/main/java/WPI/CampusMap/UI/AppUIObject.java
@@ -2,6 +2,7 @@ package WPI.CampusMap.UI;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.ScrollPane;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseListener;
@@ -17,6 +18,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
+import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
@@ -341,7 +343,7 @@ public class AppUIObject {
 
 		mainPanel.add(mapPanel);
 		mapPanel.setVisible(false);
-
+		
 		System.out.println(
 				"Image Size X: " + mapPanel.getSize().getWidth() + " Y: " + mapPanel.getSize().getHeight());
 
@@ -439,7 +441,7 @@ public class AppUIObject {
 		nodeTextField.setColumns(10);
 		
 		txtDirections.setBounds(0, 270, 220, 350);
-		directionsPanel.add(txtDirections);
+		//directionsPanel.add(txtDirections);
 		
 		final JButton btnAddDest = new JButton("+ Dest");
 		btnAddDest.setBounds(0, 76, 117, 25);
@@ -677,6 +679,11 @@ public class AppUIObject {
 				}
 			}
 		});
+		
+		JScrollPane directionsPane = new JScrollPane(txtDirections);
+		directionsPane.setBounds(txtDirections.getBounds());
+		directionsPane.setVisible(true);
+		directionsPanel.add(directionsPane);
 		
 		//mapPanel.addMouseListener(mouseClick);
 		
