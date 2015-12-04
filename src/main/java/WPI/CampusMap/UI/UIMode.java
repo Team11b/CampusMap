f0ee0b0b50ce8982dcf6244d47e5b7f50b5bb1c9
g4vin <hayesgav@gmail.com>
2015-12-04 11:53:28 -0500

    More stuff

---
+++ src/main/java/WPI/CampusMap/UI/UIMode.java
@@ -0,0 +1,38 @@
+package WPI.CampusMap.UI;
+
+public class UIMode {
+	//Singleton
+	private static UIMode instance;
+	
+	public static UIMode getInstance()
+	{
+		return instance;
+	}
+	
+	//other members of class go here
+	private String currentMode;
+	public static final String DEV_MODE = "devmode";
+	public static final String USER_MODE = "usermode";
+	
+	public UIMode(){
+		this.instance = this;
+		this.currentMode = USER_MODE;
+	}
+	
+	public String getCurrentMode(){
+		return currentMode;
+	}	
+	
+	public void switchCurrentMode(){
+		if(currentMode.equals(USER_MODE)){
+			currentMode = DEV_MODE;
+			DevMode.getInstance().onDevModeEntered();
+			//Calls button wrapper
+		}			
+		else{
+			currentMode = USER_MODE;
+			//Calls button wrapper
+		}
+	}
+
+}
