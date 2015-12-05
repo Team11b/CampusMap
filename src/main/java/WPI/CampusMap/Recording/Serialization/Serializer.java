21a29907909bbae202f778dfd0b8d6a380b3416e
Jacob Zizmor <jlzizmor@wpi.edu>
2015-12-04 21:59:11 -0500

    added loading serializer for RealMap

--- src/main/java/WPI/CampusMap/Recording/Serialization/Serializer.java
+++ src/main/java/WPI/CampusMap/Recording/Serialization/Serializer.java
@@ -12,43 +12,46 @@ import WPI.CampusMap.Backend.Core.Map.ProxyMap;
 import WPI.CampusMap.Backend.Core.Map.RealMap;
 
 public class Serializer {
-	
+
 	private static final String folder = "serialized/";
 	private static final String folderProxy = Serializer.folder + "proxy/";
 	private static final String folderReal = Serializer.folder + "real/";
 	private static final String fileType = ".ser";
-	
+
 	/**
-	 * Saves the ProxyMap to a serial file
-	 * Also saves the corresponding RealMap to a serial file
-	 * @param tosave ProxyMap to save
+	 * Saves the ProxyMap to a serial file Also saves the corresponding RealMap
+	 * to a serial file
+	 * 
+	 * @param tosave
+	 *            ProxyMap to save
 	 */
 	public static void save(ProxyMap tosave) {
-		
+
 		// save proxy map
 		try {
-			FileOutputStream fileOut = new FileOutputStream(Serializer.folderProxy + tosave.getName() + Serializer.fileType);
+			FileOutputStream fileOut = new FileOutputStream(
+					Serializer.folderProxy + tosave.getName() + Serializer.fileType);
 			ObjectOutputStream out = new ObjectOutputStream(fileOut);
-			
+
 			out.writeObject(tosave);
-			
+
 			out.close();
 			fileOut.close();
-		}
-		catch (FileNotFoundException f) {
+		} catch (FileNotFoundException f) {
 			System.out.println("NOT SAVED");
 			f.printStackTrace();
-		}
-		catch (IOException i) {
+		} catch (IOException i) {
 			System.out.println("NOT SAVED");
 			i.printStackTrace();
 		}
 	}
-	
+
 	/**
-	 * Loads a ProxyMap from a serial file
-	 * Adds the ProxyMap to the HashMap in AllMaps
-	 * @param mapName name of the map to load
+	 * Loads a ProxyMap from a serial file Adds the ProxyMap to the HashMap in
+	 * AllMaps
+	 * 
+	 * @param mapName
+	 *            name of the map to load
 	 * @return a ProxyMap which represents mapName
 	 */
 	public static ProxyMap proxyLoad(String mapName) {
@@ -56,33 +59,59 @@ public class Serializer {
 			ProxyMap pm;
 			FileInputStream fileIn = new FileInputStream(Serializer.folderProxy + mapName + Serializer.fileType);
 			ObjectInputStream in = new ObjectInputStream(fileIn);
-			
+
 			pm = (ProxyMap) in.readObject();
-			
+
 			in.close();
 			fileIn.close();
-			
+
 			AllMaps.addMap(pm);
 			return pm;
-		}
-		catch (FileNotFoundException f) {
+		} catch (FileNotFoundException f) {
 			System.out.println("NOT LOADED");
 			f.printStackTrace();
 		} catch (IOException i) {
+			System.out.println("NOT LOADED");
 			i.printStackTrace();
 		} catch (ClassNotFoundException c) {
+			System.out.println("NOT LOADED");
 			c.printStackTrace();
 		}
+
 		return null;
 	}
-	
+
 	/**
 	 * Loads a RealMap from a serial file
-	 * @param mapName name of the map to load
+	 * 
+	 * @param mapName
+	 *            name of the map to load
 	 * @return a RealMap which represents mapName
 	 */
 	public static RealMap realLoad(String mapName) {
-		throw new UnsupportedOperationException("Real load not yet implemented.");
+		try {
+			RealMap rm;
+			FileInputStream fileIn = new FileInputStream(Serializer.folderReal + mapName + Serializer.fileType);
+			ObjectInputStream in = new ObjectInputStream(fileIn);
+
+			rm = (RealMap) in.readObject();
+
+			in.close();
+			fileIn.close();
+
+			return rm;
+		} catch (FileNotFoundException f) {
+			System.out.println("NOT LOADED");
+			f.printStackTrace();
+		} catch (IOException i) {
+			System.out.println("NOT LOADED");
+			i.printStackTrace();
+		} catch (ClassNotFoundException c) {
+			System.out.println("NOT LOADED");
+			c.printStackTrace();
+		}
+
+		return null;
 	}
 
 }
