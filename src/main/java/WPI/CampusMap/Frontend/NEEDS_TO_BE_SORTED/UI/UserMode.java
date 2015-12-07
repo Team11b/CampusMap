f88f11c3ddd944d84167468336f25f26dcc49446
Will Spurgeon <wespurgeon@wpi.edu>
2015-12-07 11:17:14 -0500

    Email code is in place. It keeps failing to send though.

--- src/main/java/WPI/CampusMap/Frontend/NEEDS_TO_BE_SORTED/UI/UserMode.java
+++ src/main/java/WPI/CampusMap/Frontend/NEEDS_TO_BE_SORTED/UI/UserMode.java
@@ -4,11 +4,18 @@ import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
+import java.io.UnsupportedEncodingException;
 import java.util.Properties;
 
 import javax.mail.*;
+import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
 
 import com.twilio.sdk.TwilioRestException;
 
@@ -74,48 +81,24 @@ public class UserMode extends UIMode{
     	System.out.println("TXT");
 	}
     
-    public static void onEmail(){
-    	System.out.println("Email");
-    	// Recipient's email ID needs to be mentioned.
-        String to = "wespurgeon@wpi.edu";
-
-        // Sender's email ID needs to be mentioned
-        String from = "will@spurgeonworld.com";
-
-        // Assuming you are sending email from localhost
-        String host = "localhost";
-
-        // Get system properties
-        Properties properties = System.getProperties();
-
-        // Setup mail server
-        properties.setProperty("mail.smtp.host", host);
-
-        // Get the default Session object.
-        Session session = Session.getDefaultInstance(properties);
-
-        try{
-           // Create a default MimeMessage object.
-           MimeMessage message = new MimeMessage(session);
-
-           // Set From: header field of the header.
-           message.setFrom(new InternetAddress(from));
-
-           // Set To: header field of the header.
-           message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-
-           // Set Subject: header field
-           message.setSubject("This is the Subject Line!");
-
-           // Now set the actual message
-           message.setText("This is actual message");
-
-           // Send message
-           Transport.send(message);
-           System.out.println("Sent message successfully....");
-        }catch (MessagingException mex) {
-           mex.printStackTrace();
-        }
+    public static void onEmail() {
+    	Email email = new SimpleEmail();
+    	email.setHostName("smtp.googlemail.com");
+    	email.setSSL(true);
+    	email.setSSLOnConnect(true);
+    	email.setSmtpPort(587);
+    	email.setAuthenticator(new DefaultAuthenticator("team0011b@gmail.com", "SoftEng15"));
+    	email.setSSLOnConnect(true);
+    	try {
+    		email.setFrom("team0011b@gmail.com");
+        	email.setSubject("TestMail");
+			email.setMsg("This is a test mail ... :-)");
+			email.addTo("wespurgeon@wpi.edu");
+	    	email.send();
+		} catch (EmailException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
     
     public static void onSMS(){
