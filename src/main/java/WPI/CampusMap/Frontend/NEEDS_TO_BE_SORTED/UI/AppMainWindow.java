a561ef5d68ed45915416b91f6db464bb78b62982
Will Spurgeon <wespurgeon@wpi.edu>
2015-12-07 21:48:23 -0500

    Made changes for the Route Me button to work.

--- src/main/java/WPI/CampusMap/Frontend/NEEDS_TO_BE_SORTED/UI/AppMainWindow.java
+++ src/main/java/WPI/CampusMap/Frontend/NEEDS_TO_BE_SORTED/UI/AppMainWindow.java
@@ -43,325 +43,311 @@ import java.awt.event.KeyEvent;
 import javax.swing.KeyStroke;
 import java.awt.event.InputEvent;
 
-public class AppMainWindow extends JFrame implements Runnable
-{
+public class AppMainWindow extends JFrame implements Runnable {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 7669431755757020202L;
-	
+
 	private JLabel taskName;
 	private JProgressBar progressBar;
 	private Thread renderThread;
-	
+
 	private UIMode currentMode;
 	private AppUserModeControl userPanel;
 	private AppDevModeControl devPanel;
 	private final Action devModeAction = new DevModeAction();
 	private final Action printAction = new PrintAction();
-	
+
 	private Panel infoArea = new Panel();
-	
+
 	public AppMainWindow() {
 		super();
-		
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		setSize(new Dimension(1200, 800));
-		//setMinimumSize(new Dimension(600, 400));
+		// setMinimumSize(new Dimension(600, 400));
 		setLocation(200, 200);
 		getContentPane().setLayout(new BorderLayout(0, 0));
-		
+
 		JSplitPane splitPane = new JSplitPane();
 		splitPane.setResizeWeight(1);
 		splitPane.setDividerSize(5);
 		getContentPane().add(splitPane, BorderLayout.CENTER);
-		
-		
+
 		splitPane.setRightComponent(infoArea);
 		infoArea.setLayout(new BorderLayout(0, 0));
 		infoArea.setMinimumSize(new Dimension(300, 200));
-		
+
 		userPanel = new AppUserModeControl(this);
 		devPanel = new AppDevModeControl(this);
 		infoArea.add(userPanel, BorderLayout.CENTER);
-		
+
 		MapPanel mapPanel = new MapPanel();
 		splitPane.setLeftComponent(mapPanel);
-		
+
 		Panel bottomPane = new Panel();
 		getContentPane().add(bottomPane, BorderLayout.SOUTH);
 		bottomPane.setLayout(new BorderLayout(0, 0));
-		
+
 		progressBar = new JProgressBar();
 		bottomPane.add(progressBar, BorderLayout.EAST);
-		
+
 		taskName = new JLabel("<Task Name>");
 		bottomPane.add(taskName, BorderLayout.WEST);
-		
+
 		Component verticalStrut = Box.createVerticalStrut(20);
 		bottomPane.add(verticalStrut, BorderLayout.CENTER);
-		
+
 		JMenuBar menuBar = new JMenuBar();
 		menuBar.setBackground(SystemColor.control);
 		getContentPane().add(menuBar, BorderLayout.NORTH);
-		
+
 		JMenu mnFile = new JMenu("File");
 		menuBar.add(mnFile);
-		
+
 		JMenuItem mntmSaveAsPdf = new JMenuItem("Save As PDF");
 		mnFile.add(mntmSaveAsPdf);
-		
+
 		JMenuItem mntmSaveAsTxt = new JMenuItem("Save As TXT");
 		mnFile.add(mntmSaveAsTxt);
-		
+
 		JMenu mnSendAs = new JMenu("Send As...");
 		mnFile.add(mnSendAs);
-		
+
 		JMenuItem mntmEmail = new JMenuItem("Email");
 		mntmEmail.addActionListener(UserMode.emailAction);
 		mnSendAs.add(mntmEmail);
-		
+
 		JMenuItem mntmSms = new JMenuItem("SMS");
 		mntmSms.addActionListener(UserMode.SMSAction);
 		mnSendAs.add(mntmSms);
-		
+
 		JMenuItem mntmPrint = new JMenuItem("Print");
 		mntmPrint.setAction(printAction);
 		mnFile.add(mntmPrint);
-		
+
 		JMenu mnRouting = new JMenu("Routing");
 		menuBar.add(mnRouting);
-		
+
 		JMenu mnIndooroutdoor = new JMenu("Indoor/Outdoor");
 		mnRouting.add(mnIndooroutdoor);
-		
+
 		JRadioButtonMenuItem rdbtnmntmPreferIndoor = new JRadioButtonMenuItem("Prefer Indoor");
 		mnIndooroutdoor.add(rdbtnmntmPreferIndoor);
-		
+
 		JRadioButtonMenuItem rdbtnmntmPreferOutdoor = new JRadioButtonMenuItem("Prefer Outdoor");
 		mnIndooroutdoor.add(rdbtnmntmPreferOutdoor);
-		
+
 		JRadioButtonMenuItem rdbtnmntmUseWeather = new JRadioButtonMenuItem("Use Weather");
 		mnIndooroutdoor.add(rdbtnmntmUseWeather);
-		
+
 		JMenu mnMaps = new JMenu("Maps");
 		menuBar.add(mnMaps);
-		
+
 		JMenu mnAk = new JMenu("AK");
 		mnMaps.add(mnAk);
-		
+
 		JMenuItem mntmFirstFloor = new JMenuItem("First Floor");
 		mnAk.add(mntmFirstFloor);
-		
+
 		JMenu mnSettings = new JMenu("Settings");
 		menuBar.add(mnSettings);
-		
+
 		JCheckBoxMenuItem chckbxmntmDevMode = new JCheckBoxMenuItem("Dev Mode");
 		chckbxmntmDevMode.setAction(devModeAction);
 		mnSettings.add(chckbxmntmDevMode);
-		
+
 		renderThread = new Thread(this, "Render Thread");
 		renderThread.start();
-		
+
 		setVisible(true);
 	}
-	
+
 	/**
 	 * Gets the current UI mode.
+	 * 
 	 * @return The current UI mode.
 	 */
-	public UIMode getUIMode()
-	{
+	public UIMode getUIMode() {
 		return currentMode;
 	}
-	
+
 	/**
 	 * Gets the current UI mode as a DevMode.
+	 * 
 	 * @return The current UI mode as a DevMode, null if not in dev mode.
 	 */
-	public DevMode getDevMode()
-	{
-		if(currentMode instanceof DevMode)
-			return (DevMode)currentMode;
+	public DevMode getDevMode() {
+		if (currentMode instanceof DevMode)
+			return (DevMode) currentMode;
 		return null;
 	}
-	
+
 	/**
 	 * Gets the current UI mode as a UserMode.
+	 * 
 	 * @return The current UI mode as a UserMode, null if not in user mode.
 	 */
-	public UserMode getUserMode()
-	{
-		if(currentMode instanceof UserMode)
-			return (UserMode)currentMode;
+	public UserMode getUserMode() {
+		if (currentMode instanceof UserMode)
+			return (UserMode) currentMode;
 		return null;
 	}
-	
-	public void changeToDevMode()
-	{
+
+	public void changeToDevMode() {
 		Container parent = infoArea;
 		parent.remove(userPanel);
 		parent.add(devPanel);
 		parent.revalidate();
 		parent.repaint();
-		
+
 		currentMode = new DevMode(this);
 		currentMode.onModeEntered();
 	}
-	
-	public void changeToUserMode()
-	{
+
+	public void changeToUserMode() {
 		Container parent = infoArea;
 		parent.remove(devPanel);
 		parent.add(userPanel);
 		parent.revalidate();
 		parent.repaint();
-		
+
 		currentMode = new UserMode(this);
 		currentMode.onModeEntered();
 	}
-	
+
 	/**
-	 * Adds a map to be registered by the UI. This will add the map to all required lists.
-	 * @param map The map to be registered by the UI.
+	 * Adds a map to be registered by the UI. This will add the map to all
+	 * required lists.
+	 * 
+	 * @param map
+	 *            The map to be registered by the UI.
 	 */
-	public void addMap(IMap map)
-	{
+	public void addMap(IMap map) {
 		throw new UnsupportedOperationException("not implemented");
 	}
-	
+
 	/**
 	 * Clears all registered maps.
 	 */
-	public void clearMaps()
-	{
+	public void clearMaps() {
 		throw new UnsupportedOperationException("not implemented");
 	}
-	
+
 	/**
-	 * Forces the map to switch to a new map. This method acts like the drop down menu selected a new map, so loading should not occur on the backend.
+	 * Forces the map to switch to a new map. This method acts like the drop
+	 * down menu selected a new map, so loading should not occur on the backend.
+	 * 
 	 * @param map
 	 */
-	public void forceMapSwitch(IMap map)
-	{
+	public void forceMapSwitch(IMap map) {
 		throw new UnsupportedOperationException("not implemented");
 	}
-	
+
 	/**
 	 * Adds a destination to the destination list.
-	 * @param point The point that represents the destination.
+	 * 
+	 * @param point
+	 *            The point that represents the destination.
 	 */
-	public void addDestination(IPoint point)
-	{
+	public void addDestination(IPoint point) {
 		throw new UnsupportedOperationException("not implemented");
 	}
-	
+
 	/**
 	 * Clears the destination list.
 	 */
-	public void clearDestinations()
-	{
+	public void clearDestinations() {
 		throw new UnsupportedOperationException("not implemented");
 	}
-	
+
 	/**
 	 * Sets the route from path planning.
-	 * @param path The new route.
+	 * 
+	 * @param path
+	 *            The new route.
 	 */
-	public void setRoute(Path path)
-	{
+	public void setRoute(Path path) {
 		throw new UnsupportedOperationException("not implemented");
 	}
-	
+
 	/**
 	 * Removes the current route in the GUI without loading in another one.
 	 */
-	public void clearRoute()
-	{
+	public void clearRoute() {
 		throw new UnsupportedOperationException("not implemented");
 	}
-	
+
 	/**
 	 * Called when a point has been selected in dev mode.
-	 * @param selectedPoint The graphical point that has been selected.
+	 * 
+	 * @param selectedPoint
+	 *            The graphical point that has been selected.
 	 */
-	public void devPointSelected(DevPointGraphicsObject selectedPoint)
-	{
+	public void devPointSelected(DevPointGraphicsObject selectedPoint) {
 		throw new UnsupportedOperationException("not implemented");
 	}
-	
+
 	/**
 	 * Clears the selection from the UI.
 	 */
-	public void devClearPointSelection()
-	{
+	public void devClearPointSelection() {
 		throw new UnsupportedOperationException("not implemented");
 	}
-	
+
 	/**
 	 * Called when an edge has been selected in dev mode.
-	 * @param selectedEdge The selected edge.
+	 * 
+	 * @param selectedEdge
+	 *            The selected edge.
 	 */
-	public void devEdgeSelected(DevEdgeGraphicsObject selectedEdge)
-	{
+	public void devEdgeSelected(DevEdgeGraphicsObject selectedEdge) {
 		throw new UnsupportedOperationException("not implemented");
 	}
 
 	@Override
-	public void run()
-	{
-		while(true)
-		{
-			try 
-			{
+	public void run() {
+		while (true) {
+			try {
 				Thread.sleep(33);
-			}
-			catch (InterruptedException e)
-			{
+			} catch (InterruptedException e) {
 				return;
 			}
-			
+
 			repaint();
 		}
 	}
-	
+
 	@SuppressWarnings("serial")
-	private class DevModeAction extends AbstractAction
-	{
-		public DevModeAction()
-		{
+	private class DevModeAction extends AbstractAction {
+		public DevModeAction() {
 			putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_D, InputEvent.CTRL_MASK));
 			putValue(NAME, "Dev Mode");
 			putValue(SHORT_DESCRIPTION, "Switch to and from dev mode.");
 		}
-		
-		public void actionPerformed(ActionEvent e)
-		{
+
+		public void actionPerformed(ActionEvent e) {
 			JCheckBoxMenuItem item = (JCheckBoxMenuItem) e.getSource();
-			
-			if(item.isSelected())
-			{
+
+			if (item.isSelected()) {
 				changeToDevMode();
-			}
-			else
-			{
+			} else {
 				changeToUserMode();
 			}
 		}
 	}
-	
-	
+
 	@SuppressWarnings("serial")
-	private class PrintAction extends AbstractAction
-	{
+	private class PrintAction extends AbstractAction {
 		public PrintAction() {
 			putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_P, InputEvent.CTRL_MASK));
 			putValue(NAME, "Print");
 			putValue(SHORT_DESCRIPTION, "Prints the route to a printer.");
 		}
-		public void actionPerformed(ActionEvent e)
-		{
+
+		public void actionPerformed(ActionEvent e) {
 		}
 	}
 }
--- src/main/java/WPI/CampusMap/Frontend/NEEDS_TO_BE_SORTED/UI/AppUserModeControl.java
+++ src/main/java/WPI/CampusMap/Frontend/NEEDS_TO_BE_SORTED/UI/AppUserModeControl.java
@@ -20,26 +20,24 @@ import javax.swing.tree.DefaultMutableTreeNode;
 import java.awt.Choice;
 import java.awt.Component;
 
-public class AppUserModeControl extends JComponent
-{
-	AppMainWindow window;
+public class AppUserModeControl extends JComponent {
+	public static AppMainWindow window;
 	private JTree tree = new JTree();
-	
+
 	@SuppressWarnings("serial")
-	public AppUserModeControl(AppMainWindow window) 
-	{
+	public AppUserModeControl(AppMainWindow window) {
 		this.window = window;
-		
+
 		SpringLayout springLayout = new SpringLayout();
 		setLayout(springLayout);
-		
+
 		Choice comboBox = new Choice();
 		comboBox.add("AK");
 		comboBox.add("FL");
 		comboBox.add("Campus Map");
 		springLayout.putConstraint(SpringLayout.NORTH, comboBox, 35, SpringLayout.NORTH, this);
 		add(comboBox);
-		
+
 		PointList scrollPane = new PointList();
 		springLayout.putConstraint(SpringLayout.NORTH, scrollPane, 80, SpringLayout.NORTH, this);
 		springLayout.putConstraint(SpringLayout.WEST, comboBox, 0, SpringLayout.WEST, scrollPane);
@@ -47,75 +45,84 @@ public class AppUserModeControl extends JComponent
 		springLayout.putConstraint(SpringLayout.WEST, scrollPane, 10, SpringLayout.WEST, this);
 		springLayout.putConstraint(SpringLayout.EAST, scrollPane, -10, SpringLayout.EAST, this);
 		add(scrollPane);
-		
+
 		JButton routeButton = new JButton("Route Me!");
 		routeButton.setToolTipText("Get turn-by-turn directions between each point");
 		springLayout.putConstraint(SpringLayout.SOUTH, scrollPane, -20, SpringLayout.NORTH, routeButton);
 		springLayout.putConstraint(SpringLayout.NORTH, routeButton, 373, SpringLayout.NORTH, this);
 		springLayout.putConstraint(SpringLayout.WEST, routeButton, 54, SpringLayout.WEST, this);
 		springLayout.putConstraint(SpringLayout.EAST, routeButton, -61, SpringLayout.EAST, this);
-		routeButton.setIcon(new ImageIcon(AppUserModeControl.class.getResource("/javax/swing/plaf/metal/icons/ocean/expanded.gif")));
+		routeButton.setIcon(new ImageIcon(
+				AppUserModeControl.class.getResource("/javax/swing/plaf/metal/icons/ocean/expanded.gif")));
 		routeButton.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		add(routeButton);
 		routeButton.addActionListener(new RouteMeActionListener());
-		
+
 		Label mapLabel = new Label("Map");
 		springLayout.putConstraint(SpringLayout.WEST, mapLabel, 0, SpringLayout.WEST, comboBox);
 		springLayout.putConstraint(SpringLayout.SOUTH, mapLabel, 0, SpringLayout.NORTH, comboBox);
 		add(mapLabel);
-		
+
 		Label label = new Label("Destinations");
 		springLayout.putConstraint(SpringLayout.WEST, label, 10, SpringLayout.WEST, this);
 		springLayout.putConstraint(SpringLayout.SOUTH, label, 0, SpringLayout.NORTH, scrollPane);
 		add(label);
-		
+
 		JSeparator separator = new JSeparator();
 		springLayout.putConstraint(SpringLayout.NORTH, separator, 19, SpringLayout.SOUTH, routeButton);
 		springLayout.putConstraint(SpringLayout.WEST, separator, 0, SpringLayout.WEST, this);
 		springLayout.putConstraint(SpringLayout.SOUTH, separator, 27, SpringLayout.SOUTH, routeButton);
 		springLayout.putConstraint(SpringLayout.EAST, separator, 0, SpringLayout.EAST, this);
 		add(separator);
-		
+
 		JScrollPane scrollPane_1 = new JScrollPane();
 		springLayout.putConstraint(SpringLayout.NORTH, scrollPane_1, 25, SpringLayout.SOUTH, separator);
 		springLayout.putConstraint(SpringLayout.WEST, scrollPane_1, 10, SpringLayout.WEST, this);
 		springLayout.putConstraint(SpringLayout.SOUTH, scrollPane_1, -10, SpringLayout.SOUTH, this);
 		springLayout.putConstraint(SpringLayout.EAST, scrollPane_1, 0, SpringLayout.EAST, comboBox);
 		add(scrollPane_1);
-		
+
 		Label label_1 = new Label("Directions");
 		springLayout.putConstraint(SpringLayout.SOUTH, label_1, 0, SpringLayout.NORTH, scrollPane_1);
 		springLayout.putConstraint(SpringLayout.WEST, label_1, 10, SpringLayout.WEST, this);
-		
+
 		tree.setRootVisible(false);
-		tree.setModel(new DefaultTreeModel(
-			new DefaultMutableTreeNode("Directions") {
-				{
-					DefaultMutableTreeNode node_1;
-					node_1 = new DefaultMutableTreeNode("AK");
-						node_1.add(new DefaultMutableTreeNode("Turn left"));
-						node_1.add(new DefaultMutableTreeNode("Go straight 20 feet"));
-					add(node_1);
-				}
+		tree.setModel(new DefaultTreeModel(new DefaultMutableTreeNode("Directions") {
+			{
+				DefaultMutableTreeNode node_1;
+				node_1 = new DefaultMutableTreeNode("AK");
+				node_1.add(new DefaultMutableTreeNode("Turn left"));
+				node_1.add(new DefaultMutableTreeNode("Go straight 20 feet"));
+				add(node_1);
 			}
-		));
+		}));
 		scrollPane_1.setViewportView(tree);
 		add(label_1);
 	}
-	
-	public static String getDirections(){
-		//TODO: Get the actual directions.
-		return "Here are some directions";
+
+	public static String getDirections() {
+		return RouteMeActionListener.getDirections();
 	}
-	
-	private class RouteMeActionListener implements ActionListener
-	{
-		@Override
-		public void actionPerformed(ActionEvent e) 
-		{
+
+	private static class RouteMeActionListener implements ActionListener {
+		private static AppMainWindow mainWindow;
+
+		public static String getDirections() {
+			// TODO: Get the actual directions.
 			
+			System.out.println("Getting directions");
+			return "Here are some directions";
 		}
-		
+
+		public RouteMeActionListener() {
+			this.mainWindow = window;
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			getDirections();
+		}
+
 	}
 
 	/**
