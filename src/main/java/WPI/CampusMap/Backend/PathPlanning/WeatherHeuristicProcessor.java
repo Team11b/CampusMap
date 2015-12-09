47e97f7827a8f738ee4b721c704c9b292c1f1ab4
jlzizmor <jlzizmor@wpi.edu>
2015-12-09 00:54:13 -0500

    developed weather based heuristic

--- src/main/java/WPI/CampusMap/Backend/PathPlanning/WeatherHeuristicProcessor.java
+++ src/main/java/WPI/CampusMap/Backend/PathPlanning/WeatherHeuristicProcessor.java
@@ -1,17 +1,49 @@
 package WPI.CampusMap.Backend.PathPlanning;
 
+import WPI.CampusMap.Backend.AdditionalFeatures.InternetFeatures.Weather.WeatherAnalysis;
+
 public class WeatherHeuristicProcessor extends NodeProcessor {
+	private boolean usingWeather;
+	private boolean outside;
+	private boolean pref;
+	
+	//TODO check that this is right
+	private static final String campus = "CampusMap";
+	
+	private static final float modifier = (float) 100.0;
+	
 
 	public WeatherHeuristicProcessor() {
 	}
 	
-	public WeatherHeuristicProcessor(NodeProcessor child) {
-		this.child = child;
+	public WeatherHeuristicProcessor(NodeProcessor child, boolean usingWeather, boolean outside, boolean pref) {
+		super(child);
+		this.usingWeather = usingWeather;
+		this.outside = outside;
+		this.pref = pref;
 	}
 
 	@Override
 	protected void onProcessNode(Node node, Node goal) {
-		// TODO Auto-generated method stub
+		if (this.usingWeather) {
+			node.modifyHeuristicCost((float)WeatherAnalysis.getWeatherScore());
+		}
+		else if (pref && outside) {
+			if (node.getPoint().getMap().equals(WeatherHeuristicProcessor.campus)) {
+				node.modifyHeuristicCost(WeatherHeuristicProcessor.modifier);
+			}
+			else {
+				node.modifyHeuristicCost((-1) * WeatherHeuristicProcessor.modifier);
+			}
+		}
+		else if (pref && (!(outside))) {
+			if (node.getPoint().getMap().equals(WeatherHeuristicProcessor.campus)) {
+				node.modifyHeuristicCost((-1) * WeatherHeuristicProcessor.modifier);
+			}
+			else {
+				node.modifyHeuristicCost(WeatherHeuristicProcessor.modifier);
+			}
+		}
 
 	}
 
