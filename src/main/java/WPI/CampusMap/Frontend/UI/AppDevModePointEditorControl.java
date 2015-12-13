b74fc852eed9e5509a8e93727c5eab5808cb5971
Benny Peake <bennypeake@gmail.com>
2015-12-13 18:50:17 -0500

    Point location editor added

--- src/main/java/WPI/CampusMap/Frontend/UI/AppDevModePointEditorControl.java
+++ src/main/java/WPI/CampusMap/Frontend/UI/AppDevModePointEditorControl.java
@@ -13,15 +13,20 @@ import java.awt.event.ItemListener;
 import javax.swing.JComponent;
 import javax.swing.JSeparator;
 import javax.swing.SpringLayout;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
+import WPI.CampusMap.Backend.Core.Coordinate.Coord;
 import WPI.CampusMap.Backend.Core.Point.RealPoint;
 import WPI.CampusMap.Frontend.Graphics.Dev.DevPointGraphicsObject;
 
 import javax.swing.JPanel;
 import java.awt.GridLayout;
 import javax.swing.JToggleButton;
+import javax.swing.SpinnerNumberModel;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.JSpinner;
 
 public class AppDevModePointEditorControl extends JComponent {
 	DevPointGraphicsObject point;
@@ -92,6 +97,34 @@ public class AppDevModePointEditorControl extends JComponent {
 		springLayout.putConstraint(SpringLayout.WEST, separator_1, 0, SpringLayout.WEST, this);
 		springLayout.putConstraint(SpringLayout.EAST, separator_1, 0, SpringLayout.EAST, this);
 		add(separator_1);
+		
+		JSpinner xCoordField = new JSpinner();
+		xCoordField.setModel(new SpinnerNumberModel(point.getWorldPosition().getX(), Double.MIN_VALUE, Double.MAX_VALUE, 0.1));
+		xCoordField.addChangeListener(new XCoordChangedAction());
+		springLayout.putConstraint(SpringLayout.NORTH, xCoordField, 40, SpringLayout.SOUTH, typeSelector);
+		springLayout.putConstraint(SpringLayout.WEST, xCoordField, 0, SpringLayout.WEST, typeSelector);
+		springLayout.putConstraint(SpringLayout.EAST, xCoordField, -10, SpringLayout.EAST, this);
+		add(xCoordField);
+		
+		JLabel lblXlabel = new JLabel("x: ");
+		springLayout.putConstraint(SpringLayout.NORTH, lblXlabel, 0, SpringLayout.NORTH, xCoordField);
+		springLayout.putConstraint(SpringLayout.SOUTH, lblXlabel, 0, SpringLayout.SOUTH, xCoordField);
+		springLayout.putConstraint(SpringLayout.EAST, lblXlabel, 0, SpringLayout.WEST, xCoordField);
+		add(lblXlabel);
+		
+		JSpinner yCoordField = new JSpinner();
+		yCoordField.setModel(new SpinnerNumberModel(point.getWorldPosition().getY(), Double.MIN_VALUE, Double.MAX_VALUE, 0.1));
+		yCoordField.addChangeListener(new YCoordChangedAction());
+		springLayout.putConstraint(SpringLayout.NORTH, yCoordField, 10, SpringLayout.SOUTH, xCoordField);
+		springLayout.putConstraint(SpringLayout.WEST, yCoordField, 0, SpringLayout.WEST, xCoordField);
+		springLayout.putConstraint(SpringLayout.EAST, yCoordField, 0, SpringLayout.EAST, xCoordField);
+		add(yCoordField);
+		
+		JLabel lblYlabel = new JLabel("y: ");
+		springLayout.putConstraint(SpringLayout.NORTH, lblYlabel, 0, SpringLayout.NORTH, yCoordField);
+		springLayout.putConstraint(SpringLayout.SOUTH, lblYlabel, 0, SpringLayout.SOUTH, yCoordField);
+		springLayout.putConstraint(SpringLayout.EAST, lblYlabel, 0, SpringLayout.WEST, yCoordField);
+		add(lblYlabel);
 	}
 	
 	private class PointTypeChangedAction implements ItemListener
@@ -129,6 +162,28 @@ public class AppDevModePointEditorControl extends JComponent {
 				field.setText(currentText);
 			else
 				currentText = field.getText();
+		}
+	}
+	
+	private class XCoordChangedAction implements ChangeListener
+	{
+		@Override
+		public void stateChanged(ChangeEvent e)
+		{
+			JSpinner spinner = (JSpinner)e.getSource();
+			Double x = (Double)spinner.getValue();
+			point.getRepresentedObject().setCoord(new Coord(x.floatValue(), point.getWorldPosition().getY()));
+		}
+	}
+	
+	private class YCoordChangedAction implements ChangeListener
+	{
+		@Override
+		public void stateChanged(ChangeEvent e)
+		{
+			JSpinner spinner = (JSpinner)e.getSource();
+			Double y = (Double)spinner.getValue();
+			point.getRepresentedObject().setCoord(new Coord(point.getWorldPosition().getX(), y.floatValue()));
 		}
 	}
 
