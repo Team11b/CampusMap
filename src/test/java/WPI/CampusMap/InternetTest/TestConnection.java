af3808c56022092915ed94802b246ed1699dd53f
g4vin <hayesgav@gmail.com>
2015-12-13 18:18:19 -0500

    added twitter test, always fails unless private twitter info entered.

--- src/test/java/WPI/CampusMap/InternetTest/TestConnection.java
+++ src/test/java/WPI/CampusMap/InternetTest/TestConnection.java
@@ -5,14 +5,21 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 
+import WPI.CampusMap.Backend.AdditionalFeatures.InternetFeatures.Twitter.Information.TwitterInformation;
 import WPI.CampusMap.Backend.AdditionalFeatures.InternetFeatures.Weather.Wunderground;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.UnknownHostException;
+import java.util.List;
 
 public class TestConnection {
+	
 	@Before
 	public void initialize() {
 
@@ -44,6 +51,27 @@ public class TestConnection {
 		assertTrue(true); 
 		}		
 		catch(Exception e){ //thrown when weather is null
+			assertTrue(false);
+		}
+	}
+	
+	@Test
+	public void testPullTwitter(){
+		//needs private twitter information
+		String consumerKeyStr = TwitterInformation.getPublicKey();
+		String consumerSecretStr = TwitterInformation.getPrivateKey();
+		String accessTokenStr = TwitterInformation.getPublicAccessToken();
+		String accessTokenSecretStr = TwitterInformation.getPrivateAccessToken();
+		try{
+		Twitter twitter = new TwitterFactory().getInstance();
+		twitter.setOAuthConsumer(consumerKeyStr, consumerSecretStr);
+		AccessToken accessToken = new AccessToken(accessTokenStr, accessTokenSecretStr);
+
+		twitter.setOAuthAccessToken(accessToken);
+		List<Status> statuses = twitter.getMentionsTimeline();		
+		assertTrue(true); 
+		}		
+		catch(Exception e){ //thrown when statuses is null or there isn't a private key.
 			assertTrue(false);
 		}
 	}
