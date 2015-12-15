5899b5119c51554fed2a516579c76f05645b1e46
jlzizmor <jlzizmor@wpi.edu>
2015-12-15 16:12:34 -0500

    added check to weather about for not connecting to internet

--- src/main/java/WPI/CampusMap/Backend/AdditionalFeatures/InternetFeatures/Weather/Wunderground.java
+++ src/main/java/WPI/CampusMap/Backend/AdditionalFeatures/InternetFeatures/Weather/Wunderground.java
@@ -1,5 +1,7 @@
 package WPI.CampusMap.Backend.AdditionalFeatures.InternetFeatures.Weather;
 
+import javax.swing.JOptionPane;
+import javax.ws.rs.ProcessingException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.Response;
@@ -12,34 +14,67 @@ import com.google.gson.Gson;
  */
 public class Wunderground extends WundergroundAbstract {
 	private ResponseContainer resp;
+	private boolean hasWeather;
+	private static final String error = "Weather cannot be recieved.";
 
 	public Wunderground() {
 		BASE_URI = "http://api.wunderground.com/api/2901b67aa5967692/conditions/q/MA/Worcester.json";
+
 		Client client = ClientBuilder.newClient();
 		targetService = client.target(BASE_URI);
-		Response results = targetService.request().get();
-		String responseAsString = results.readEntity(String.class);
-		Gson gson = new Gson();
-		resp = gson.fromJson(responseAsString, ResponseContainer.class);
+
+		Response results = null;
+
+		try {
+			System.out.println("here");
+			results = targetService.request().get();
+		} catch (ProcessingException e) {
+			hasWeather = false;
+			JOptionPane.showMessageDialog(null, Wunderground.error, "ERROR", JOptionPane.ERROR_MESSAGE);
+			System.out.println(Wunderground.error);
+		}
+
+		if (results != null) {
+			String responseAsString = results.readEntity(String.class);
+			Gson gson = new Gson();
+			resp = gson.fromJson(responseAsString, ResponseContainer.class);
+			hasWeather = true;
+		}
 	}
 
 	public double getTempC() {
+		if (!(hasWeather)) {
+			return 0.0;
+		}
 		return resp.getCurrent_observation().getTempC();
 	}
 
 	public double getTempF() {
+		if (!(hasWeather)) {
+			return 0.0;
+		}
 		return resp.getCurrent_observation().getTempF();
 	}
 
 	public String getWeather() {
+		if (!(hasWeather)) {
+			return Wunderground.error;
+		}
 		return resp.getCurrent_observation().getWeather();
 	}
 
 	public double getWindMPH() {
+		if (!(hasWeather)) {
+			return 0.0;
+		}
 		return resp.getCurrent_observation().getWindMPH();
 	}
-	
+
 	public String toString() {
 		return "" + this.getWeather() + " @ " + this.getTempF() + " degF & " + this.getWindMPH() + " MPH";
+	}
+
+	public boolean hasWeather() {
+		return this.hasWeather;
 	}
 }
