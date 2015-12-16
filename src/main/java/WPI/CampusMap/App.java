35afc2fe62c4a57528ac76106f09a1b6f8909686
bpeake13 <bennypeake@gmail.com>
2015-12-16 09:27:36 -0500

    Added file logging

--- src/main/java/WPI/CampusMap/App.java
+++ src/main/java/WPI/CampusMap/App.java
@@ -1,5 +1,9 @@
 package WPI.CampusMap;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
 import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 
@@ -13,12 +17,28 @@ public class App {
 
 	public static void main(String[] args)
 	{
+		try
+		{
+			FileOutputStream fs = new FileOutputStream("log.txt");
+			PrintStream printStream = new PrintStream(fs);
+			
+			System.setOut(printStream);
+		} 
+		catch (Exception e1)
+		{
+			return;
+		}
+		
+		System.out.println("Starting app...");
+		
 		try {
 			if (System.getProperty("os.name").startsWith("Windows"))
 				UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
 			@SuppressWarnings("unused")
 			AppMainWindow uiInstance = new AppMainWindow();
-		} catch (Exception e) {
+		} catch (Exception e)
+		{
+			e.printStackTrace();
 			JOptionPane.showMessageDialog(null, e.getLocalizedMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
 		}
 	}
