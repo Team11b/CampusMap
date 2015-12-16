8441ff163bfc266f9f03c535a1f8f0a1678ef8fc
Will Spurgeon <wespurgeon@wpi.edu>
2015-12-15 20:11:52 -0500

    The useless bottom pane of the app window is gone now.

--- src/main/java/WPI/CampusMap/Frontend/UI/AppMainWindow.java
+++ src/main/java/WPI/CampusMap/Frontend/UI/AppMainWindow.java
@@ -118,11 +118,11 @@ public class AppMainWindow extends JFrame implements Runnable {
 		splitPane.setLeftComponent(mapPanel);
 
 		Panel bottomPane = new Panel();
-		getContentPane().add(bottomPane, BorderLayout.SOUTH);
+		//getContentPane().add(bottomPane, BorderLayout.SOUTH);
 		bottomPane.setLayout(new BorderLayout(0, 0));
 
 		progressBar = new JProgressBar();
-		bottomPane.add(progressBar, BorderLayout.EAST);
+		//bottomPane.add(progressBar, BorderLayout.EAST);
 
 		taskName = new JLabel("");
 		bottomPane.add(taskName, BorderLayout.WEST);
